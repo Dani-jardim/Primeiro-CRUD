c668bab9ec08b7adf29ccb996c538ced7576d80a
Daniela Jardim <danialvesdesigner@gmail.com>
2023-08-30 15:37:42 -0300

    criando nova rota

--- src/routes/techmaromba-route.js
+++ src/routes/techmaromba-route.js
@@ -1,1 +1,6 @@
-//TODO: Importar o express, o controller e instanciar o Router para chamar os metodos HTTP passando o endereço e função do CRUD referente 
+const express  = require("express")
+const router = express.Router()
+
+router.get("/all", findAll)
+
+router.post("/add", addNew)
