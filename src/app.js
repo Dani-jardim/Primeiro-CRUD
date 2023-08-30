49874e7bd3cdc665dc802eb036174132b749db93
Daniela Jardim <danialvesdesigner@gmail.com>
2023-08-30 17:51:41 -0300

    conectando banco de dados

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 const express = require("express")
 const cors = require("cors")
 const techMarombaRoute = require("./routes/techmaromba-route")
+const mongoose = require("./database/dbConnect")
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
+
+mongoose.connect()
+
 app.use("techMarombaRoute", techMarombaRoute)
 
 module.exports = app 
--- src/database/dbConnect.js
+++ src/database/dbConnect.js
@@ -1,1 +1,16 @@
-//TODO: Inserir configurações para integrar o MongoDB
+
+const mongoose = require("mongoose")
+
+const connect = async () => {
+  try {
+    mongoose.connect("mongodb+srv://danialvesdesigner:gcfxiE0XSmk1JkoC@cluster0.vuenoti.mongodb.net/", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    console.log("Database connected")
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+module.exports = {connect}
