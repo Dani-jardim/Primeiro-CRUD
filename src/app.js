0d979e6b26c8499408f4e1aa9e35596be312cbbf
Daniela Jardim <danialvesdesigner@gmail.com>
2023-08-30 16:46:41 -0300

    ajustes de tipo

--- src/app.js
+++ src/app.js
@@ -4,8 +4,8 @@ const techMarombaRoute = require("./routes/techmaromba-route")
 
 const app = express()
 
-app.use(express.json[])
-app.use(cors[])
+app.use(express.json())
+app.use(cors())
 app.use("techMarombaRoute", techMarombaRoute)
 
 module.exports = app 
